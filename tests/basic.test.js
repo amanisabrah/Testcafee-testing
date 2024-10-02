809f0299359eff48149484fc9a080bbc01ba8d56
Your Name <amanisabrah@gmail.com>
2024-10-02 11:07:01 +0300

    add the test speed with the range 0.01 'slow' to 1'fast'

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -3,9 +3,22 @@ import { Selector } from "testcafe"
 
 fixture `Devexpress Testcafe testing application`
     .page `https://devexpress.github.io/testcafe/example/`
+    .before(async t =>{
+        //test setup goes here, axait runDatabaseReset(), await seedTestData()
+    })
+    .beforeEach(async t => {
+        //run befor each test 
+    })
+    .after(async t =>{
+        //cleaning test data, Logging and sending data tp monitoring systems
+    })
+    .afterEach(async t => {
+        //runs after each test
+    })
 
 test('Test Case1', async t =>{
     //here goes test cafe code
+    await t.setTestSpeed(0.01)
     await t.typeText('#developer-name', 'Amani')
     await t.click('#submit-button')
 
